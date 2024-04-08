5a3ea473d8beba634156845ae30242520c23b8b2
tendertree <tendertree@gmail.com>
2024-04-08 17:46:51 +0900

    fix: path reslove problems

---
+++ src/components/hello.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@ui/shadcn/button";
+import Hi from "./hi";
+
+export default function Name() {
+    return (
+        <div>This is my name and..
+            <Button>arst</Button>
+
+        </div>
+    )
+}
---
+++ src/components/hi.tsx
@@ -0,0 +1,9 @@
+export default function Hi() {
+    return (
+        <div>
+            this is simple Hi component!
+        </div>
+    )
+
+}
+
